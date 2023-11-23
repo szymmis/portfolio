d62e1d35f2ec5a02d6aeeb79002ff1f3fa6b45d2
Szymon Miśtal <mistalzaq@gmail.com>
2023-11-23 22:04:59 +0100

    Figure out how to render components dynamically

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -17,7 +17,7 @@ export const PROJECTS: Project[] = [
     imageSrc: "images/quiz_1.png",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus leo dolor, tincidunt ut pulvinar bibendum, sodales at ipsum. Sed lacinia sagittis dui, placerat hendrerit dolor commodo non. Mauris at bibendum urna. Nunc accumsan leo in magna dapibus scelerisque. Nam convallis, ante ut dignissim laoreet, enim ante euismod augue, eget varius ipsum tellus nec augue.",
-    techs: ["typescript"],
+    techs: ["typescript", "react"],
   },
   {
     title: "Vite Express",
