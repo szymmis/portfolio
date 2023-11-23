b04f484d6eb65d3ff398e978828a439ed65574db
Szymon Miśtal <mistalzaq@gmail.com>
2023-11-23 21:30:48 +0100

    Display projects entries as tiles with images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,6 +5,7 @@ export const PROJECTS: Project[] = [
     emoji: "⚡",
     title: "Vite Express",
     href: "https://github.com/szymmis/vite-express",
+    imageSrc: "images/image.png",
     stars: "330+",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus leo dolor, tincidunt ut pulvinar bibendum, sodales at ipsum. Sed lacinia sagittis dui, placerat hendrerit dolor commodo non. Mauris at bibendum urna. Nunc accumsan leo in magna dapibus scelerisque. Nam convallis, ante ut dignissim laoreet, enim ante euismod augue, eget varius ipsum tellus nec augue.",
@@ -13,6 +14,7 @@ export const PROJECTS: Project[] = [
   {
     title: "Vite Express",
     href: "https://github.com/szymmis/vite-express",
+    imageSrc: "images/quiz_1.png",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus leo dolor, tincidunt ut pulvinar bibendum, sodales at ipsum. Sed lacinia sagittis dui, placerat hendrerit dolor commodo non. Mauris at bibendum urna. Nunc accumsan leo in magna dapibus scelerisque. Nam convallis, ante ut dignissim laoreet, enim ante euismod augue, eget varius ipsum tellus nec augue.",
     techs: ["typescript"],
--- src/ts/Project.ts
+++ src/ts/Project.ts
@@ -2,6 +2,7 @@ export interface Project {
   emoji?: string;
   title: string;
   href: string;
+  imageSrc?: string;
   stars?: string | number;
   description: string;
   techs: string[];
